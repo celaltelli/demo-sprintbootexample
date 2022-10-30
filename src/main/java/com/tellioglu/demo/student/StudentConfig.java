e0afa7e9beb466d83551abfc2f27fef861ad38e3
celaltelli <celaltelli@gmail.com>
2022-10-30 18:22:12 +0300

    initial commit for spring boot example

---
+++ src/main/java/com/tellioglu/demo/student/StudentConfig.java
@@ -0,0 +1,26 @@
+package com.tellioglu.demo.student;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+
+        return args -> {
+
+            Student celal = new Student("Celal", "celal.telli@gmail.com", LocalDate.of(1987, Month.MAY,1));
+            Student hatice = new Student("Hatice", "hatice.telli@gmail.com", LocalDate.of(1998, Month.MARCH,1));
+
+            repository.saveAll(List.of(celal, hatice));
+        };
+    }
+    
+}
