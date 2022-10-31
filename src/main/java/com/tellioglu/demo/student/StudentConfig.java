858c0f8ff5881bcfc89107723c8d3d4184efea55
celaltelli <celaltelli@gmail.com>
2022-10-31 09:15:14 +0300

    update 1

--- src/main/java/com/tellioglu/demo/student/StudentConfig.java
+++ src/main/java/com/tellioglu/demo/student/StudentConfig.java
@@ -12,15 +12,15 @@ import org.springframework.context.annotation.Configuration;
 public class StudentConfig {
 
     @Bean
-    CommandLineRunner commandLineRunner(StudentRepository repository){
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
 
         return args -> {
 
-            Student celal = new Student("Celal", "celal.telli@gmail.com", LocalDate.of(1987, Month.MAY,1));
-            Student hatice = new Student("Hatice", "hatice.telli@gmail.com", LocalDate.of(1998, Month.MARCH,1));
+            Student celal = new Student("Celal", "celal.telli@gmail.com", LocalDate.of(1987, Month.MAY, 1));
+            Student hatice = new Student("Hatice", "hatice.telli@gmail.com", LocalDate.of(1998, Month.MARCH, 1));
 
             repository.saveAll(List.of(celal, hatice));
         };
     }
-    
+
 }
